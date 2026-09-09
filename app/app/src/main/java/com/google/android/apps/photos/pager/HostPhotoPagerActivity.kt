c04bd6347aa91dc14cb7c04d1fd83d6efcdafce6
kmd66apple-cell <kmd66apple@gmail.com>
2026-09-10 08:52:35 +0900

    Update HostPhotoPagerActivity.kt

--- app/app/src/main/java/com/google/android/apps/photos/pager/HostPhotoPagerActivity.kt
+++ app/app/src/main/java/com/google/android/apps/photos/pager/HostPhotoPagerActivity.kt
@@ -1,10 +1,10 @@
 package com.google.android.apps.photos
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class HostPhotoPagerActivity : Activity() {
+class HostPhotoPagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
